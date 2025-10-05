0a7a926fa6ec3a0ff26830d4a4510fc68123c762
Cowlife <56398689+Cowlife@users.noreply.github.com>
2025-10-05 16:13:51 +0100

    Introdutory files to start the project
    Just the usual Hello World style files that need to be used if the project wants to be up and running.

---
+++ Backend_Folders/src/main/java/org/example/backend_folders/BackendFoldersApplication.java
@@ -0,0 +1,13 @@
+package org.example.backend_folders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFoldersApplication.class, args);
+	}
+
+}
---
+++ Backend_Folders/src/test/java/org/example/backend_folders/BackendFoldersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backend_folders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFoldersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
